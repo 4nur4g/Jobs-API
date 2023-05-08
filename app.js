a2fa356ce1e519b65af18ab12a738fd34375c4ee
Anurag Pradhan <anuragp936@gmail.com>
2023-05-08 23:07:59 +0530

    Register token res

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+// extra packages
+
+// ConnectDB
+
+const ConnectDB = require('./db/connect');
+
+// routes
+const authRoutes = require('./routes/auth');
+const jobsRoutes = require('./routes/jobs');
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/jobs', jobsRoutes);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await ConnectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
+const login = async (req, res) => {
+  res.send('Login user');
+};
+
+module.exports = {
+  register,
+  login,
+};
